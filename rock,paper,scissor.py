2c39c78eec1f08a0569bcf0a8d81cba10d7ab4e1
gauri448 <76466991+gauri448@users.noreply.github.com>
2020-12-22 12:22:01 +0530

    Add files via upload

---
+++ rock,paper,scissor.py
@@ -0,0 +1,24 @@
+import random
+
+# player1 = user
+# player2 = computer
+def game():
+    player1 = input("choose any one: 'R' for Rock, 'P' for paper,'S' for scissors\n")
+    player2 = random.choice(['R', 'P', 'S'])
+
+    if player1 == player2:
+        return 'It\'s a tie'
+
+   # R > S, S > P, P > R
+    if match_win(player1, player2):
+       return 'You won'
+    else:
+       return 'You lost'
+
+
+def match_win(player1, player2):
+    # return true if playe1 wins
+
+    if (player1 == 'R' and player2 == 'S') or (player1 == 'S' and player2 == 'P') or (player1 == 'P' and player2 == 'R'):
+        return True
+print(game())
